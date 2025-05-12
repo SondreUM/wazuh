12593c1a9ac1e15b80d04f4a0a7b24c1980cda6a
SondreUM <sondreum@gmail.com>
2025-05-13 00:24:28 +0200

    fix: improved internal memory mgmt

--- src/client-agent/detect/detect.h
+++ src/client-agent/detect/detect.h
@@ -6,9 +6,9 @@
 #include "state.h"
 
 /* detection definitions  */
-#define DETECT_VERSION "0.1.3"
+#define DETECT_VERSION "0.1.5"
 // Maximum number of HREs to process concurrently
-#define MAX_HRE 10
+#define MAX_HRE 20
 // Maximum duration of the log buffer in seconds
 #define MAX_LOG_DURATION 60
 // Maximum number of rules to that can be loaded
@@ -24,12 +24,11 @@
 /* OSSEC queue event type
  * docs: https://documentation.wazuh.com/4.10/development/message-format.html#input-logs
  * 1-byte event type. It defines the decoding mode for Analysis daemon.
-    The most common queue types are:
- * 1 Local file log, including Syslog messages, Windows event logs, outputs from commands, OpenSCAP results and custom
- logs.
+ *
+ * The most common queue types are:
+ * 1 Local file log, including Syslog messages, Windows event logs, outputs from commands, OpenSCAP results and custom logs.
  * 2 Remote Syslog messages, received by the Syslog server at Remote daemon.
- * 4 Secure messages. They are events from Remote daemon to Analysis daemon, that contain a standard OSSEC message plus
- the source agent ID.
+ * 4 Secure messages. They are events from Remote daemon to Analysis daemon, that contain a standard OSSEC message plus the source agent ID.
  * 8 Syscheck event. Analysis daemon parses it using the Syscheck decoder.
  * 9 Rootcheck event. Analysis daemon parses it using the Rootcheck decoder.
  */
